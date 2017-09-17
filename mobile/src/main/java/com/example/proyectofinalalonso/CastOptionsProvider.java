d8505fa49e9699e0659c33a7f0154a92abfc7c95
Alonso <alonsosjumper@gmail.com>
2017-09-17 15:03:03 +0200

    CastOptionsProvider

---
+++ mobile/src/main/java/com/example/proyectofinalalonso/CastOptionsProvider.java
@@ -0,0 +1,27 @@
+package com.example.proyectofinalalonso;
+
+import android.content.Context;
+
+import com.google.android.gms.cast.CastMediaControlIntent;
+import com.google.android.gms.cast.framework.CastOptions;
+import com.google.android.gms.cast.framework.OptionsProvider;
+import com.google.android.gms.cast.framework.SessionProvider;
+
+import java.util.List;
+
+/**
+ * Created by Alonso on 16/09/2017.
+ */
+
+public class CastOptionsProvider implements OptionsProvider {
+    @Override
+    public CastOptions getCastOptions(Context appContext) {
+        CastOptions castOptions = new CastOptions.Builder() .setReceiverApplicationId("4F8B3483") .build();
+        return castOptions;
+    }
+
+    @Override
+    public List<SessionProvider> getAdditionalSessionProviders(Context context) {
+        return null;
+    }
+}
