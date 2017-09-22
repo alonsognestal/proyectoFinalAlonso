526c087c92ddae23dda05ee79f369f21244b1ed2
Alonso <alonsosjumper@gmail.com>
2017-09-22 22:19:53 +0200

    Como estaba el BackgroundAudioService

--- mobile/src/main/java/com/example/proyectofinalalonso/BackgroundAudioService.java
+++ mobile/src/main/java/com/example/proyectofinalalonso/BackgroundAudioService.java
@@ -94,7 +94,7 @@ public class BackgroundAudioService extends MediaBrowserServiceCompat implements
             super.onPause();
 
             if (mMediaPlayer.isPlaying()) {
-                mMediaPlayer.stop();
+                mMediaPlayer.pause();
                 setMediaPlaybackState(PlaybackStateCompat.STATE_PAUSED);
                 try {
                     showPausedNotification();
